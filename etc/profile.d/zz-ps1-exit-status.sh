5bfa4812c0bdd3a7ddcf924e7cb587f5ff772bd0
Lauri Korts-Pärn <lauri@korc.jp>
2016-01-14 21:31:50 -0500

    update error status prompt setting

--- etc/profile.d/zz-ps1-exit-status.sh
+++ etc/profile.d/zz-ps1-exit-status.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-_set_prompt_exit_status () { 
-  case "$1" in 
-      0) _last_exit_status="" ;;
-      *) _last_exit_status="$1|" ;;
-  esac
+_set_prompt_exit_status () {
+  _last_exit_status="$?"
+  test ! "$_last_exit_status" = 0 || _last_exit_status=""
+  return $_last_exit_status
+}
+
+_add_status_to_ps1() {
+  local status_var_name="${1:-_last_exit_status}"
+  local red="$( (tput setaf 1; tput bold) | sed -e 's/\x1b/\\e/g' )"
+  local sgr0="$(tput sgr0 | sed -e 's/\x1b/\\e/g')"
   case "$PS1" in
-    *_last_exit_status*) ;;
-    *) PS1="\$_last_exit_status$PS1"
+    *$status_var_name*) ;;
+    *) PS1="\${$status_var_name:+\[$red\]\$$status_var_name\[$sgr0\]|}$PS1" ;;
   esac
 }
 
 case "$PROMPT_COMMAND" in
   *_set_prompt_exit_status*) ;;
-  *) PROMPT_COMMAND="_set_prompt_exit_status \"\$?\"; $PROMPT_COMMAND"
+  *)
+    _add_status_to_ps1 _last_exit_status
+    PROMPT_COMMAND="_set_prompt_exit_status; $PROMPT_COMMAND"
+  ;;
 esac
-
