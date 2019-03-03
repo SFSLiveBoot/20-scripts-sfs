54f6e810153d2e6513bfd7394058c150d259f729
Lauri Korts-Pärn <lauri@korc.jp>
2019-03-03 13:19:23 -0500

    update connect-public-wifi (psk support)

--- opt/bin/connect-public-wifi.sh
+++ opt/bin/connect-public-wifi.sh
@@ -9,33 +9,44 @@ choose1() {
     *) echo "$1";;
   esac
 }
+run() { (set -x; "$@"); }
 
 set -e
-(set -x; nmcli radio wifi on)
+run nmcli radio wifi on
 while test -z "$ssid";do
-  (set -x; nmcli device wifi list)
+  run nmcli device wifi list
   echo "Unique SSIDs:"
   nmcli -t -f ssid device wifi list | grep -v '^--$' | sort -u | sed -e 's/^/  /'
   ssid="$(prompt "SSID (enter for rescan)")"
 done
 : "${cname:=$(prompt "Connection name" "$ssid")}"
-echo "Signal Freq      BSSID"
-nmcli -m multiline -t -f ssid,signal,freq,bssid device wifi list | grep -A4 -Fx "SSID:$ssid" | grep -e SIGNAL -e FREQ -e BSSID | cut -f2- -d: | xargs -d'\n' printf " %d    %s  %s\n" | sort -n
+if nmcli -t -f connection.type connection show "$cname";then op="modify"; else op="add"; fi
+echo "Signal Freq      BSSID              Security"
+nmcli -m multiline -t -f ssid,signal,freq,bssid,security device wifi list | grep -A5 -Fx "SSID:$ssid" | grep -e SIGNAL -e FREQ -e BSSID -e SECURITY | cut -f2- -d: | xargs -d'\n' printf " %d    %s  %s  %s\n" | sort -n
 : "${bssid:=$(prompt "BSSID")}"
-(set -x; nmcli device)
-: "${iface:=$(choose1 "$(nmcli -t -f type,device device | grep ^wifi: | cut -f2- -d:)" "WiFi interface")}"
+test -z "$bssid" || : "${security:=$(nmcli -m multiline -t -f bssid,security device wifi list | grep -A1 -Fx "BSSID:$bssid" | grep ^SECURITY: | sort -u | cut -f2 -d:)}"
+case " $security " in
+  *" WPA1 "*|*" WPA2 "*)
+  : "${key_mgmt:=wpa-psk}"
+  : "${wpa_psk:=$(prompt "Pre-shared key")}"
+  ;;
+  "  ") echo "WARNING: no link-level security possible" >&2;;
+  *) echo "WARNING: Unhandled security method: '$security'" >&2 ;;
+esac
 test -n "$never_default" || case "$(prompt "Use as default route? [Y/n]")" in Y*|y*|"") ;; *) never_default=1;; esac
-(set -x; nmcli connection add \
+run nmcli connection $op \
   type wifi \
   con-name "$cname" \
   cloned-mac random \
   ssid "$ssid" \
-  ifname "$iface" \
+  ifname "*" \
   ipv4.dhcp-send-hostname false \
   autoconnect no \
   ${bssid:+wifi.bssid "$bssid"} \
-  ${never_default:+ipv4.never-default yes ipv4.ignore-auto-dns yes ipv6.never-default yes ipv6.ignore-auto-dns yes})
-(set -x; nmcli connection up "$cname")
-mac="$(nmcli -t -f GENERAL.HWADDR device show "$iface" | cut -f2- -d:)"
+  ${never_default:+ipv4.never-default yes ipv4.ignore-auto-dns yes ipv6.never-default yes ipv6.ignore-auto-dns yes} \
+  ${key_mgmt:+wifi-sec.key-mgmt $key_mgmt} ${wpa_psk:+wifi-sec.psk "$wpa_psk"}
+run nmcli connection up "$cname"
+: "${iface:=$(choose1 "$(run nmcli -t -f GENERAL.DEVICES connection show "$cname" | cut -f2- -d:)" "WiFi interface")}"
+mac="$(run nmcli -t -f GENERAL.HWADDR device show "$iface" | cut -f2- -d:)"
 echo "Fixating parameters (mac=$mac)"
-(set -x; nmcli connection modify "$cname" cloned-mac "$mac")
+run nmcli connection modify "$cname" cloned-mac "$mac"
